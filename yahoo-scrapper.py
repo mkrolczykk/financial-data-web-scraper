23ce8318e86ec355b573d640b29f3ad281fe1ba8
mkrolczykk <m.krolczyk66@gmail.com>
2024-05-09 16:20:38 +0200

    mkrolczyk - created json serializator and csv reader/writer

--- yahoo-scrapper.py
+++ yahoo-scrapper.py
@@ -6,7 +6,6 @@ import json
 import time
 import urllib.request
 from urllib.error import HTTPError
-import pandas as pd
 from datetime import datetime
 from multiprocessing.dummy import Pool
 from dependencies.LoggingCustomFormatter import LoggingCustomFormatter
@@ -39,6 +38,48 @@ def _check_www_conn():
         return False
 
 
+def serialize_json(data):
+    def serialize(obj):
+        if obj is None:
+            return 'null'   # convert python None to json null
+        elif isinstance(obj, dict):
+            serialized_dict = '{'
+            for key, value in obj.items():
+                serialized_dict += f'"{key}": {serialize(value)}, '
+            serialized_dict = serialized_dict.rstrip(', ')
+            serialized_dict += '}'
+            return serialized_dict
+        elif isinstance(obj, list):
+            serialized_list = '['
+            for item in obj:
+                serialized_list += serialize(item) + ', '
+            serialized_list = serialized_list.rstrip(', ')
+            serialized_list += ']'
+            return serialized_list
+        elif isinstance(obj, bool):
+            return 'true' if obj else 'false'  # Serialize bool values to true or false
+        elif isinstance(obj, str):
+            return f'"{obj}"'
+        else:
+            return str(obj)
+
+    return serialize(data)
+
+
+def save_json(data, file_path):
+    with open(file_path, 'w') as file:
+        serialized_data = serialize_json(data)
+        file.write(serialized_data)
+
+
+def save_csv(data, file_path, headers):
+    with open(file_path, 'w') as file:
+        file.write(','.join(headers) + '\n')    # write headers
+        for row in data:
+            row_str = ','.join(map(str, row))
+            file.write(row_str + '\n')
+
+
 def get_historical_data(query_url, json_path, csv_path) -> None:
     stock_id = query_url.split("&period")[0].split("symbol=")[1]
 
@@ -54,9 +95,13 @@ def get_historical_data(query_url, json_path, csv_path) -> None:
         with urllib.request.urlopen(query_url) as url:
             parsed_json = json.loads(url.read().decode())
 
-    except HTTPError:
-        log.error("HTTP Error 429: Too Many Requests")
-    except:
+    except HTTPError as e:
+        if e.code == 429:
+            log.error("HTTP Error 429: Too Many Requests")
+        else:
+            log.error(f"HTTP Error {e.code}")
+    except Exception as e:
+        print(e)
         log.warning("Historical data of " + stock_id + " cannot be found")
         return
 
@@ -64,8 +109,7 @@ def get_historical_data(query_url, json_path, csv_path) -> None:
         if os.path.exists(json_path + stock_id + '.json') and os.stat(json_path + stock_id + '.json').st_size != 0:
             os.remove(json_path + stock_id + '.json')
 
-        with open(json_path + stock_id + '.json', 'w') as outfile:
-            json.dump(parsed_json, outfile, indent=4)
+        save_json(parsed_json, json_path + stock_id + '.json')
 
         try:
             Date = []
@@ -79,15 +123,18 @@ def get_historical_data(query_url, json_path, csv_path) -> None:
             Close = parsed_json['chart']['result'][0]['indicators']['quote'][0]['close']
             Adjusted_Close = parsed_json['chart']['result'][0]['indicators']['adjclose'][0]['adjclose']
 
-            df = pd.DataFrame(list(zip(Date, Low, Open, Volume, High, Close, Adjusted_Close)),
-                              columns=['Date', 'Low', 'Open', 'Volume', 'High', 'Close', 'Adjusted Close'])
+            data = []
+            for d, low, opn, vol, high, close, adj_close in zip(Date, Low, Open, Volume, High, Close, Adjusted_Close):
+                data.append([d, low, opn, vol, high, close, adj_close])
 
             if os.path.exists(csv_path + stock_id + '.csv'):
                 os.remove(csv_path + stock_id + '.csv')
-            df.to_csv(csv_path + stock_id + '.csv', sep=',', index=None)
+
+            headers = ['Date', 'Low', 'Open', 'Volume', 'High', 'Close', 'Adjusted Close']
+            save_csv(data, csv_path + stock_id + '.csv', headers)
             log.info(f"Historical data of {stock_id} save SUCCESS")
-        except:
-            log.error(f"Retrieving historical data of {stock_id} FAIL")
+        except Exception as e:
+            log.error(f"Retrieving historical data of {stock_id} FAIL: {str(e)}")
 
         return None
 
@@ -101,14 +148,15 @@ def main(tickers_csv_filepath: str, interval: str):
     if not os.path.isdir(csv_output_path):
         os.makedirs(csv_output_path)
 
-    df = pd.read_csv(tickers_csv_filepath)
-    log.info(f"Total stocks to proceed: {len(df)}")
-
     query_urls = []
+    with open(tickers_csv_filepath, 'r') as file:
+        lines = file.readlines()
+        for line in lines[1:]:  # Skip header
+            ticker = line.strip().split(',')[0]  # Assuming 'ticker' is in first column
+            query_urls.append(
+                f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?symbol={ticker}&period1=0&period2=9999999999&interval={interval}&includePrePost=true&events=div%2Csplit")
 
-    for ticker in df['Ticker']:
-        query_urls.append(
-            f"https://query1.finance.yahoo.com/v8/finance/chart/{ticker}?symbol={ticker}&period1=0&period2=9999999999&interval={interval}&includePrePost=true&events=div%2Csplit")
+    log.info(f"Total stocks to proceed: {len(query_urls)}")
 
     with Pool(processes=8) as pool:
         pool.starmap(get_historical_data, zip(query_urls, itertools.repeat(json_output_path), itertools.repeat(csv_output_path)))
